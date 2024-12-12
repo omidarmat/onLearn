a59baf9d0b21dd5f23b7b605733135329f5fd057
omidarmat <omidarmat@gmail.com>
2024-12-12 17:30:31 +0330

    remove unnecessary files

--- test.ts
+++
@@ -1,9 +0,0 @@
-function map<Input, Output>(
-  arr: Input[],
-  fn: (arg: Input) => Output
-): Output[] {
-  return arr.map(fn);
-}
-
-const parsed = map(["1", "2", "3"], (n) => parseInt(n));
-console.log(parsed);
